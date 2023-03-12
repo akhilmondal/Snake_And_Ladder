0b70fab143ccc0943ee3584411e944ad7058868b
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-12 22:55:57 +0530

    Added: Program to roll the dice with random values.

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,9 +2,12 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
 
+    static int diceRoll = 0;
     public static void main(String[] args) {
 
         System.out.println("Welcome to snake and ladder game.");
+        diceRoll = ((int) (Math.random()*10)%6)+1; // Used random function to generate random numbers.
+        System.out.println(diceRoll);
         int player1Position = 0; // For player 1 initial position has declared.
     }
 }
